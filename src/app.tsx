5d04260c63c2ce8ba46df202137503cfa29f7430
wayco <waycowei@gmail.com>
2021-11-12 17:35:16 +0800

    feat: 修改模板脚本

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,32 @@
 import { Provider } from 'react-redux'
+import { Component } from 'react'
 
 import './app.scss'
 import store from './store'
+class App extends Component {
+  onLaunch () {
+    // 解决 iOS 不支持 Promise.finally 的问题
+    if (!Promise.prototype.finally) {
+      // eslint-disable-next-line
+      Promise.prototype.finally = function (onfinally?: (() => void)) {
+        let P = this.constructor
+        return this.then(
+          value => P.resolve(onfinally?.()).then(() => value),
+          reason => P.resolve(onfinally?.()).then(() => {throw reason })
+        )
+      }
+    }
+  }
 
-function App (props) {
   // props.children 是将要会渲染的页面
-  return (
-    <Provider store={store}>
-      {props.children}
-    </Provider>
-  )
+  render () {
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
+  }
 }
+
 
 export default App
